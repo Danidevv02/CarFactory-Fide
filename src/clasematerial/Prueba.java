1d9ee2d8b7ad82ea51548a431a8c48fbcf2d9c83
jogm08 <josuegomezm0808@gmail.com>
2024-02-26 11:51:25 -0600

    prueba commit

---
+++ src/clasematerial/Prueba.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clasematerial;
+
+/**
+ *
+ * @author PC
+ */
+public class Prueba {
+  //Esto es solo una prueba SE PUEDE ELIMINAR  
+}
