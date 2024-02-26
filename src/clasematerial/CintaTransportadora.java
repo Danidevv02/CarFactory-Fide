3e5a721db1d86dca8978b378568b71f1b571417b
Ale001101 <avalverde80523@ufide.ac.cr>
2024-02-26 13:10:26 -0600

    agrego funciones que aun muestrar error

--- src/clasematerial/CintaTransportadora.java
+++ src/clasematerial/CintaTransportadora.java
@@ -1,8 +1,47 @@
 package clasematerial;
+
 public class CintaTransportadora {
-    //prueba 2
-    
-    
-    
-    
+
+    Material cabeza;
+
+    public CintaTransportadora() {
+        this.cabeza = null;
+    }
+
+    public void agregarMaterial(Material material) {
+        if (cabeza == null) {
+            cabeza = Material;
+        } else {
+            Material actual = cabeza;
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = material;
+        }
+    }
+
+    public void eliminarMaterial(String nombreMaterial) {
+        if (cabeza != null) {
+            if (cabeza.nombre.equals(nombreMaterial)) {
+                cabeza = cabeza.siguiente;
+            } else {
+                Material actual = cabeza;
+                while (actual.siguiente != null && !actual.siguiente.nombre.equals(nombreMaterial)) {
+                    actual = actual.siguiente;
+                }
+                if (actual.siguiente != null) {
+                    actual.siguiente = actual.siguiente.siguiente;
+                }
+            }
+        }
+    }
+
+    public void mostrarContenido() {
+        Material actual = cabeza;
+        while (actual != null) {
+            System.out.println(actual.nombre);
+            actual = actual.siguiente;
+        }
+    }
+
 }
